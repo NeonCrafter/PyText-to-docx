623d4586c1d2d97c05302be7fed354be2056d739
sudam <sudamedirisinghe5@gmail.com>
2023-05-20 07:13:51 +0530

    added heading styles

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 from docx import Document
+from docx.shared import Pt
 class main():
     def __init__(self ,headSize = 29 , headFont = "monospace" , normalSize = 15 , normalFont = "sans serif" , inFile = "in.txt" , outFile = "out.docx"):
         self.headSize = headSize
         self.headFont = headFont
         self.normalSize = normalSize
         self.normalFont = normalFont
-        self.document = Document()
-        self.document.add_page()
+        self.document =Document()
+        self.inFile = inFile
+        self.outfile = outFile
         self.points = []
         self.headers = []
+        self.headStyle = self.document.styles["Normal"]
+        self.headStyle.font.name = self.headFont
+        self.headStyle.font.size = Pt(self.headSize)
     def readfile(self):
         file = open(self.inFile , "rt")
         self.fileContents = file.read()
         file.close()
+    def get_info(self):
+        for line in self.fileContents.splitlines():
+            if line.startswith("!!HEAD"):
+                self.headers.append(line)
+            else:
+                self.points.append(line)
+    def Write(self):
+        for line in self.fileContents.splitlines():
+            if line.startswith("!!HEAD"):
+                head = self.document.add_heading(text = line.replace("!!HEAD" , ""))
+                head.style = self.document.styles["Normal"]
+            else:
+                paragraph = self.document.add_paragraph(text = line)
+    def save(self):
+        self.document.save(self.outfile)
+        
+instance = main()
+instance.readfile()
+instance.Write()
+instance.save()
