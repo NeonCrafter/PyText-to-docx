8783278a3a5f1d0349c5d7646803fd1e9ac39d04
Mr.Unknown <sudamedirisinghe5@gmail.com>
2023-12-29 18:26:26 +0530

    initial commit

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ class main():
         self.normalSize = normalSize
         self.normalFont = normalFont
         self.document =Document()
+
         self.inFile = inFile
         self.outfile = outFile
         self.points = []
         self.headers = []
-        self.headStyle = self.document.styles["Normal"]
+
+        self.headStyle = self.document.styles["Title"]
         self.headStyle.font.name = self.headFont
         self.headStyle.font.size = Pt(self.headSize)
+
+        self.normalStyle = self.document.styles["Normal"]
+        self.normalStyle.font.name = self.normalFont
+        self.normalStyle.font.size = Pt(self.normalSize)
+
     def readfile(self):
         file = open(self.inFile , "rt")
         self.fileContents = file.read()
@@ -26,14 +33,20 @@ class main():
                 self.points.append(line)
     def Write(self):
         for line in self.fileContents.splitlines():
+            print(line)
             if line.startswith("!!HEAD"):
                 head = self.document.add_heading(text = line.replace("!!HEAD" , ""))
-                head.style = self.document.styles["Normal"]
+                head.style = self.document.styles["Title"]
+            elif line.startswith("!!IMAGE"):
+                self.document.add_picture(line.replace("!!IMAGE" , ""))
             else:
                 paragraph = self.document.add_paragraph(text = line)
+                paragraph.style = self.document.styles["Normal"]
+        
+
     def save(self):
         self.document.save(self.outfile)
-        
+
 instance = main()
 instance.readfile()
 instance.Write()
